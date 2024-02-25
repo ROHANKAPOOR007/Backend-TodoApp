d632513c7e23cf1412ed784d7e37caf8f220a576
ROHAN KAPOOR <indiarohan.kapoor@gmail.com>
2024-02-25 17:05:55 +0530

    database file edit

--- data/database.js
+++ data/database.js
@@ -4,8 +4,8 @@ export const connectDB = ()=>{
 mongoose
 .connect(process.env.MONGO_URI, {
 })
-  .then(() => {
-    console.log("Database is Connected");
+  .then((c) => {
+    console.log(`Database is Connected with ${c.connection.host}`);
   })
   .catch((e) => {
     console.log("Something went wrong", e);
